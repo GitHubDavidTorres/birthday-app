a6b9909de912b6a11aa3054a28a37e491b7d02cc
GitHubDavidTorres <david.torres.santander@gmail.com>
2021-12-15 12:37:41 -0300

    commit on master

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Socrates',
+    age: 29,
+    image:
+      'https://cdn.britannica.com/69/75569-050-7AB67C4B/herm-Socrates-half-original-Greek-Capitoline-Museums.jpg',
+  },
+  {
+    id: 2,
+    name: 'Plato',
+    age: 32,
+    image:
+      'https://cdn.britannica.com/88/149188-050-DC34842F/Plato-portrait-bust-original-Capitoline-Museums-Rome.jpg',
+  },
+  {
+    id: 3,
+    name: 'Aristotle ',
+    age: 36,
+    image:
+      'https://www.biografiasyvidas.com/monografia/aristoteles/fotos/aristoteles_420.jpg',
+  },
+  {
+    id: 4,
+    name: 'Marcus Aurelius',
+    age: 34,
+    image:
+      'https://i.guim.co.uk/img/media/c5a3cb22f4ae7b4a4c7462ef4d2d0c71cacb3c65/0_232_2789_1673/master/2789.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=d8b0aa8df7f1ebae670b860fde1e622e',
+  },
+  {
+    id: 5,
+    name: 'Augustus',
+    age: 29,
+    image:
+    'https://smarthistory.org/wp-content/uploads/2020/10/primaporta-face.jpg',
+  },
+];
